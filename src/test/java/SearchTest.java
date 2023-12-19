0c24534a60d53b07583913ae48eda6851993f0e8
Ten-go <usupov.tengiz@gmail.com>
2023-12-19 17:45:59 +0300

    search code junit

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void testSearchJUnit() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
+
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                    @Test
+                    void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                    }
+                }"""));
+    }
+}
